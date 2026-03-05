6e45a8fb46743c311d1173f1b5c9449c6a1a2a81
Barkın Arı <brkn3342@gmail.com>
2026-03-05 22:15:32 +0300

    fix: the contents of the TokenResponseDto.cs file

--- ChatAppServer/ChatAppServer.Api/Dtos/TokenResponseDto.cs
+++ ChatAppServer/ChatAppServer.Api/Dtos/TokenResponseDto.cs
@@ -1,8 +1,7 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace ChatAppServer.Api.Dtos;
 
-public record TokenResponseDto(
-    [Required] string AccessToken,
-    [Required] string RefreshToken
-);
+public class TokenResponseDto
+{
+    public required string AccessToken { get; set; }
+    public required string RefreshToken { get; set; }
+}
